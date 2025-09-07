e91de0e7eb3ba5149d26d9b8bc03d566017d54a1
Subrat Kumar Sahoo <ssubratkumar106@gmail.com>
2025-09-07 18:10:49 +0530

    add to code

--- ReversedString.c
+++ ReversedString.c
@@ -1,4 +1,42 @@
 #include <stdio.h>
+#include<conio.h>
+char text[40];
+int top = -1;
+
+void push(char c) {
+    if (top < 39 && c != '*') {
+        top++;
+        text[top] = c;
+    }
+}
+
+char pop() {
+    if (top >= 0) {
+        return text[top--];
+    }
+    return '\0';
+}
+
+int main() {
+    char ch;
+    printf("Enter a string (* to end): ");
+
+    while ((ch = getchar()) != '*' && top < 39) {
+        push(ch);
+    }
+
+    printf("\nReverse String is: ");
+    while (top >= 0) {
+        putchar(pop());
+    }
+
+    return 0;
+}
+
+
+
+
+#include <stdio.h>
 
 char text[40];
 int top = -1;
